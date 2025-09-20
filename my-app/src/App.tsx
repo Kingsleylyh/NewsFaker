a5392333e770930cc44476a24a90ffc9d0e5495d
Hoe Wei Leong <weileonghoe@gmail.com>
2025-09-20 11:35:40 +0800

    feat: change Home page to New Chat page

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
-import HomePage from "./pages/Home";
 import URLsPage from "./pages/URLs";
 import ImagesPage from "./pages/Images";
 import VideosPage from "./pages/Videos";
 import SocialMediaPage from "./pages/SMLinks";
+import NewChat from "./pages/NewChat";
 
 function App() {
   const [active, setActive] = useState("Home");
@@ -13,11 +13,11 @@ function App() {
     <div className="min-h-screen flex flex-col bg-darkBg text-white">
       <Navbar active={active} setActive={setActive} />
       <main className="flex-1 p-6">
-        {active === "Home" && <HomePage />}
-        {active === "URLs" && <URLsPage />}
+        {active === "New Chat" && <NewChat />}
+        {/* {active === "URLs" && <URLsPage />}
         {active === "Images" && <ImagesPage />}
         {active === "Videos" && <VideosPage />}
-        {active === "Social Media Links" && <SocialMediaPage />}
+        {active === "Social Media Links" && <SocialMediaPage />} */}
       </main>
     </div>
   );
